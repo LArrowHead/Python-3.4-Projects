9baffa63767b546bf18b47c10a2d6054f2e7f6cb
LArrowHead <lwaldron321@gmail.com>
2016-05-17 17:53:46 -0400

    Add files via upload

---
+++ scope_main.py
@@ -0,0 +1,20 @@
+#scope_main.py
+#by: LArrowHead
+import time
+import random
+from random import randint
+
+print('\n' * 10)
+print('\n\n\n------------------------')
+print('        Welcome to Scope!')
+print('------------------------')
+print('\n\n[Starting... Starting...]')
+print('\n\n If stuck: type help-')
+time.sleep(3)
+print('\nCTRL242 Initiation Process...')
+time.sleep(2)
+print('\nStarting Droid 242...')
+time.sleep(1)
+print('\nPlease Wait...')
+time.sleep(3)
+print('\nAcessing Available Systems...')
